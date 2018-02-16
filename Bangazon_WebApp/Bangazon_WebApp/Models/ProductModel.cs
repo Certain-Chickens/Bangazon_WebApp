662338da69ee05fc87aa8c9aa8d7558a4b16c60a
Ryan McPherson <mmcpher2@gmail.com>
2018-02-16 13:49:16 -0800

    Added all fields on ProductTypeModel and Recommendations Model

--- Bangazon_WebApp/Bangazon_WebApp/Models/ProductModel.cs
+++ Bangazon_WebApp/Bangazon_WebApp/Models/ProductModel.cs
@@ -1,11 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Bangazon_WebApp.Models
 {
-    public class Class
+    public class ProductModel
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(55, ErrorMessage = "Please shorten the product title to 55 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double Price { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        [Display(Name = "Product Category")]
+        public int ProductTypeId { get; set; }
+
+        public ProductTypeModel ProductType { get; set; }
+
+        public PhotosModel Photo { get; set; }
+
+        public RecommendationsModel Recommendations { get; set; }
+
+        public LikesModel Likes { get; set; }
+
+        public ProductRatingModel ProductRating { get; set; }
+
+        public virtual ICollection<OrderProductModel> OrderProduct { get; set; }
+
     }
 }
