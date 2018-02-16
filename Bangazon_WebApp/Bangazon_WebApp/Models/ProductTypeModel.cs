10bc0ca924115aff8673d74893cb96b726973924
Robbie Shock <rob.f.shock@gmail.com>
2018-02-16 12:31:39 -0800

    completed PaymentTypeModle

---
+++ Bangazon_WebApp/Bangazon_WebApp/Models/ProductTypeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_WebApp.Models
+{
+    public class ProductTypeModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string PaymentName { get; set; }
+        public string AccountNumber { get; set; }
+
+    }
+}
