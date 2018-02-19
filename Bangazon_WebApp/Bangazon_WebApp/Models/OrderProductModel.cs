4a14e82d67ab05942c11c4e984a1fd90003bccb1
eagobert <eagobert@hotmail.com>
2018-02-19 11:24:21 -0600

    Complete order product mmmmmodel

---
+++ Bangazon_WebApp/Bangazon_WebApp/Models/OrderProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_WebApp.Models
+{
+    public class OrderProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public ProductModel Product { get; set; }
+
+        [Required] 
+        public OrderModel Order { get; set; }
+    }
+}
