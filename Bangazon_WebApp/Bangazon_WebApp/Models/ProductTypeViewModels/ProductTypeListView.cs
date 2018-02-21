f9bd8902d2e6bd2d5bdbd44e70f3ead83339d257
eagobert <eagobert@hotmail.com>
2018-02-21 15:07:18 -0600

    product category list is working

---
+++ Bangazon_WebApp/Bangazon_WebApp/Models/ProductTypeViewModels/ProductTypeListView.cs
@@ -0,0 +1,15 @@
+﻿//Author:  Erin Agobert
+//Purpose:  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_WebApp.Models.ProductTypeViewModels
+{
+    public class ProductTypeListView
+    {
+        public ProductType ProductType { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
